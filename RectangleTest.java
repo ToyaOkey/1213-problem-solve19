bb4594a4263b76b39362ca8f8670af157b95c423
ToyaOkey <cooltoya12@gmail.com>
2025-04-14 12:01:34 -0400

    Complete Rectangle class exercise

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,9 +1,21 @@
+import org.w3c.dom.css.Rect;
+
 import java.awt.Rectangle;
 
 public class RectangleTest {
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(10, 10, 40, 30);
         System.out.println("box1: " + box1);
+
+        Rectangle box2 = new Rectangle(100, 50);
+        System.out.println("box1: " + box2);
+
+
+        box1.move(20, 20); // or box1.setLocation(20, 20);
+        box2.resize(50, 30); // or box2.setSize(50, 30);
+        System.out.println("box1: " + box1);
+        System.out.println("box2: " + box2);
+
     }
 
 }
