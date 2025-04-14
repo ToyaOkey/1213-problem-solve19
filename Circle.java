0b4f1d81adedd1d178a871a839f51631a68c49c1
ToyaOkey <cooltoya12@gmail.com>
2025-04-14 12:16:26 -0400

    Add JavaDoc to Circle class and generate documentation

--- Circle.java
+++ Circle.java
@@ -4,6 +4,8 @@
  * The Circle class represents a circle with a radius and color.
  */
 
+import java.awt.*;
+
 /**
  * This class describes a circle with a given radius and color.
  *
@@ -16,35 +18,60 @@ public class Circle {
     private Color color;
     private double area;
 
-    // The default constructor with no argument.
-    // It sets the radius and color to their default value.
+
+
+    /**
+     * Default constructor
+     * Sets the radius to 1.0 and color to red.
+     */
     public Circle() {
         radius = 1.0;
         this.color = new Color("red");
     }
 
-    // 2nd constructor with given radius and color
+    /**
+     * Constructor with given radius and color
+     *
+     * @param r the radius of the circle
+     * @param clr the color of the circle
+     */
     public Circle(double r, Color clr) {
         radius = r;
         color = clr;
     }
 
-    // A public method for retrieving the radius
+
+
+    /**
+     * A public method for retrieving the radius
+     * @return radius of circle
+     */
     public double getRadius() {
         return radius;
     }
 
-    // A public method for retrieving the color of circle
+
+    /**
+     * A public method for retrieving the color
+     * @return color of circle
+     */
     public Color getColor() {
         return color;
     }
 
-    // A public method for retrieving the area of circle
+
+    /**
+     * A public method for retrieving the area of circle
+     * @return area of circle
+     */
     public double getArea() {
         calculateArea();
         return area;
     }
-    // A private method for computing the area of circle
+
+    /**
+     * A private method for calculating the area of circle
+     */
     private void calculateArea() {
         area = radius * radius * Math.PI;
     }
